5cb1e39e15452db4638b012a040d1a135fad4a79
Ethan Schneider <EthanSchneiderGames@gmail.com>
2025-02-07 21:16:26 -0600

    Bumper Movement

--- Volume Control Pitch Perfect Too/Assets/BlockBreaker/Scripts/Paddle.cs
+++ Volume Control Pitch Perfect Too/Assets/BlockBreaker/Scripts/Paddle.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Paddle : MonoBehaviour {
     public AudioDetection detector;
     private const float LOUDNESS_SENSIBILITY = 5, THRESHOLD = 0.1f;
+    [SerializeField] GameObject left, right;
+    private GameObject bumper;
+
+    private void Start() {
+        bumper = gameObject;
+    }
 
     private void FixedUpdate() {
         float loudness = detector.GetLoudnessFromMicrophone() * LOUDNESS_SENSIBILITY;
 
         if (loudness < THRESHOLD) {
             loudness = 0;
+            Mathf.Lerp(bumper.transform.position.x, left.transform.position.x, Time.deltaTime * loudness);
         }
         else {
-
+            Mathf.Lerp(bumper.transform.position.x, right.transform.position.x, Time.deltaTime * loudness);
         }
     }
     
