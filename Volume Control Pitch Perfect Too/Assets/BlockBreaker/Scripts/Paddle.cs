0521718c4c534b32970817d462003b7bc96decab
Ethan Schneider <EthanSchneiderGames@gmail.com>
2025-02-07 21:09:53 -0600

    Metas

--- Volume Control Pitch Perfect Too/Assets/BlockBreaker/Scripts/Paddle.cs
+++ Volume Control Pitch Perfect Too/Assets/BlockBreaker/Scripts/Paddle.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Paddle : MonoBehaviour {
+    public AudioDetection detector;
+    private const float LOUDNESS_SENSIBILITY = 5, THRESHOLD = 0.1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+        float loudness = detector.GetLoudnessFromMicrophone() * LOUDNESS_SENSIBILITY;
+
+        if (loudness < THRESHOLD) {
+            loudness = 0;
+        }
+        else {
+
+        }
     }
+    
 }
