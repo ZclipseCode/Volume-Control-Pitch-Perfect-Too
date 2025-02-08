8da59f0496d02c9b2701ae94f7374df491e2e359
CameronAron <cameron.aron04@gmail.com>
2025-02-07 20:25:52 -0600

    sound shit

---
+++ Volume Control Pitch Perfect Too/Assets/AudioDetection.cs
@@ -0,0 +1,58 @@
+using JetBrains.Annotations;
+using Microsoft.Win32.SafeHandles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDetection : MonoBehaviour
+{
+    public int sampleWindow = 64;
+    public AudioClip microphoneClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MicrophoneToAudioClip();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MicrophoneToAudioClip()
+    {
+        //Get the first microphone in device list
+        string microphoneName = Microphone.devices[0];
+        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+    }
+
+    public float GetLoudnessFromMicrophone()
+    {
+        return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+    }
+
+    public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
+    {
+        int startPosition = clipPosition - sampleWindow;
+        
+        if(startPosition < 0)
+        {
+            return 0;
+        }
+
+        float[] waveData = new float[sampleWindow];
+        clip.GetData(waveData, startPosition);
+
+        //Compute loudness
+        float totalLoudness = 0;
+
+        for (int i = 0; i < sampleWindow; i++)
+        {
+            totalLoudness += Mathf.Abs(waveData[i]);
+        }
+
+        return totalLoudness / sampleWindow;
+    }
+}
